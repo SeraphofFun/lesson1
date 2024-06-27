5babf902534d291e05a6ffd50b16ae2e822eb5b4
SeraphofFun <149875292+SeraphofFun@users.noreply.github.com>
2024-06-27 21:53:13 +0400

    Add files via upload

---
+++ Lesson1/Lesson1.cpp
@@ -0,0 +1,76 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	cout << "\t Resume\n"<<"Name: Tebriz Surname: Mammadov Age: 19 Hobby: Anime\n\n\n";
+
+	cout << "   - - -\n" << " /\t \\\n" << "/\t  \\\n"<<"|\t  |\n" << "|\t  |\n" << "|\t  |\n" << "\\\t  /\n" << " \\\t /\n"<< "   - - -\n";
+
+	cout << "\"to be \n\t or not \n \t\t to be!\"\n\t\t\t /Shakespear/\n"; 
+
+	cout << "   Escape Sequence\n";
+	cout<<"\\a\t Bell (alert)\n";
+	cout << "\\b\t Backspace\n";
+	cout << "\\n\t New line\n";
+	cout << "\\r\t Carriage return\n";
+	cout << "\\t\t harizontal tab\n";
+	cout << "\\\\\t backslasj \\\n";
+	cout << "\\\"\t Double quatation mark \"\n";
+	cout << "\\\'\t Single quatation mark\n";
+
+
+	cout << " \t\t\t   *\n";
+	cout << "******* ***** ***** *****    ******\n";
+	cout << "   *\t    * *   * *   *  *\t *\n";
+	cout << "   *\t    * ****  ****   *    *\n";
+	cout << "   *\t***** *   * *   *  *   *\n";
+	cout << "   *\t***** ***** *    * *  ******\n\n";
+	cout<<"\" \'\\t\', \'\\n\',\'\\\\\', \'\\\'\', \'\\\"\' \" "<<endl<<endl;
+
+
+	cout << "\t ********************\n";
+	cout << "\t*|\t\t   **\n";
+	cout << "       * |\t\t  * *\n";
+	cout << "      *  |\t\t *  *\n";
+	cout << "     *   |\t\t*   *\n";
+	cout << "    *    |\t       *    *\n";
+	cout << "   *     |\t      *     *\n";
+	cout << "  ********************      *\n";
+	cout << "  *       |          *      *\n";
+	cout << "  *       |          *      *\n";
+	cout << "  *       |          *      *\n";
+	cout << "  *       |          *      *\n";
+	cout << "  *       |          *      *\n";
+	cout << "  *       |          *      *\n";
+	cout << "  *       |          *      *\n";
+	cout << "  *       |          *      *\n";
+	cout << "  *       |          *      *\n";
+	cout << "  *       |          *      *\n";
+	cout << "  *       |          *      *\n";
+	cout << "  *       |          *      *\n";
+	cout << "  *       |__________*______*\n";
+	cout << "  *      /           *      *\n";
+	cout << "  *     /            *     *\n";
+	cout << "  *    /             *    *\n";
+	cout << "  *   /              *   *\n";
+	cout << "  *  /               *  *\n";
+	cout << "  * /                * *\n";
+	cout << "  */                 **\n";
+	cout << "  ********************\n";
+
+
+
+
+	cout << "#include <iostream>\n";
+	cout << "using  namespace std;\n";
+	cout << "void main()\n";
+	cout << "{\n";
+	cout << "\t cout << \"C++ forever!!!\";\n";
+	cout << "}\n";
+
+
+	return 0;
+}
+
